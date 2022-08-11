f732e9ff4f4bbea9bd32fe89f15733a7bb370fc4
= <=>
2022-08-11 16:50:52 -0300

    Ejercicio de List terminado

--- 02-Sintaxis,Variables y palabras reservadas/index.js
+++ 02-Sintaxis,Variables y palabras reservadas/index.js
@@ -1,1 +1,13 @@
-aaaa
+const date = new Date("September 09 1998")
+
+const libro = { 
+    titulo : "La interpretación de los sueños" ,
+    autor: "Singmund Freud",
+    fecha : 1899,
+    url : "https://campus.open-bootcamp.com"
+}
+
+const user = ["Jonathan", 23, true, date, libro ]
+
+
+console.log (user)
