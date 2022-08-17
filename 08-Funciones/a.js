ed6ef4887a7dd6afe916aa9a047102f42b06154c
= <jblenis98@gmail.com>
2022-08-17 19:57:13 -0300

    Ejercicio 8

---
+++ 08-Funciones/a.js
@@ -0,0 +1,22 @@
+/*Crea un archivo JS que contenga las siguientes líneas
+
+- Una función sin parámetros que devuelva siempre "true"
+
+- Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+
+- Una función generadora de índices pares automáticos8*/
+
+function verdad() {
+    return true
+}
+
+function promesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+function idsPar() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
